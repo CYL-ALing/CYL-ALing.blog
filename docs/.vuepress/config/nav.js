4d92af1bb5626adc27883fe2879d3e986319d5fb
Roger0x0000 <Roger0x0000@outlook.com>
2021-10-27 22:46:56 +0800

    1.add pdf

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -29,8 +29,7 @@ module.exports = [
     link: '/technology/',
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '电源文集', link: '/pages/4c778760be26d8b3/' },
     ],
   },
   {
